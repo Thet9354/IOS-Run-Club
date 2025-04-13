73ff1fef215c59a81c856510e2eebfee94699913
Thet Pine <thetpine254@gmail.com>
2025-04-13 18:08:45 +0800

    Initial Commit

---
+++ RunClub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunClub
+//
+//  Created by Phoon Thet Pine on 13/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunClub/RunClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunClubApp.swift
+//  RunClub
+//
+//  Created by Phoon Thet Pine on 13/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RunClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
